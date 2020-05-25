ece37ba9bf2d2df02370e9897c006202bfb7dc74
mauricio <mauriciojunio@gmail.com>
2020-05-25 20:55:14 -0300

    :art: adicionado system js para resolver dependencias

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,3 +1,5 @@
+import { NegociacaoController } from './controllers/NegociacaoController';
+
 const controller = new NegociacaoController();
 //Comentario
 $('.form').submit(controller.adiciona.bind(controller));
--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,8 +1,7 @@
-namespace Views {
-    export class MensagemView extends Views.View<string>{
+import { View } from './View';
 
-        template(model: string) : string{
-            return `<p class="alert alert-info">${model}</p>`;
-        }
+export class MensagemView extends View<string>{
+    template(model: string) : string{
+        return `<p class="alert alert-info">${model}</p>`;
     }
 }
