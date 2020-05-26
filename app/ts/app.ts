fceab26b6fe5da18ca7268ae44009693b9243aaf
mauricio <mauriciojunio@gmail.com>
2020-05-25 21:06:04 -0300

    :rocket: servidor local

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,5 +1,4 @@
 import { NegociacaoController } from './controllers/NegociacaoController';
-
 const controller = new NegociacaoController();
 //Comentario
 $('.form').submit(controller.adiciona.bind(controller));
