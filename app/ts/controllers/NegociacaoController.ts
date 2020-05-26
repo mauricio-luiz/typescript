92a03791ddc313f8d1a0d64a94000ea0e7881bf2
mauricio <mauriciojunio@gmail.com>
2020-05-26 19:49:35 -0300

    :art: adicionado parametro adicional typescript

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -7,8 +7,8 @@ export class NegociacaoController {
     private _inputQuantidade: JQuery;
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
-    private _negociacoesView = new NegociacoesView('#negociacoesView');
-    private _mensagemView = new MensagemView('#mensagemView');
+    private _negociacoesView = new NegociacoesView('#negociacoesView', true);
+    private _mensagemView = new MensagemView('#mensagemView', true);
 
     constructor(){
         this._inputData = $("#data");
