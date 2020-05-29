b4a0a66afea420d316be794eae773b6552166208
mauricio <mauriciojunio@gmail.com>
2020-05-29 20:18:54 -0300

    :art: foi construido uma interface generica que extende varias outras interfaces para ser implementas nos models

--- app/ts/models/index.ts
+++ app/ts/models/index.ts
@@ -1,4 +1,6 @@
 export * from './Negociacao';
 export * from './Negociacoes';
 export * from './NegociacaoParcial';
-export * from './imprimivel';
+export * from './Imprimivel';
+export * from './Igualavel';
+export * from './MeuObjeto';
